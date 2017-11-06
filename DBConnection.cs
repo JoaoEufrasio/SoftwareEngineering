b80307fae3e13b96643625cb12564927efe08164
GiovanniPug <33149451+GiovanniPug@users.noreply.github.com>
2017-11-06 16:18:04 +0000

    Add files via upload

---
+++ DBConnection.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class DBConnection
+    {
+        //attributes
+        private static DBConnection _instance;
+
+        private static string connectionString;
+
+        private SqlConnection connectionToDB;
+
+        private SqlDataAdapter dataAdapter;
+
+
+        //properties
+        public static string ConnectionStr
+        {
+            set
+            {
+                connectionString = value;
+            }
+        }
+
+        //methods
+        public static DBConnection getDBConnectionInstance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+
+            return _instance;
+        }
+
+        // Open the connection
+        public void openConnection()
+        {
+            
+            ConnectionStr = "";
+            // create the connection to the database as an instance of SqlConnection
+            connectionToDB = new SqlConnection(connectionString);
+
+            //open connection
+            connectionToDB.Open();
+        }
+
+        public void closeConnection()
+        {
+            connectionToDB.Close();
+        }
+
+
+       //create a data set for a certain request
+        public DataSet getDataSet(String sqlStatement)
+        {
+            DataSet dataSet = new DataSet();
+
+            //open connection
+            openConnection();
+
+            //create the data adapter object
+            dataAdapter = new SqlDataAdapter(sqlStatement, connectionToDB);
+
+            //fill in the data set
+            dataAdapter.Fill(dataSet);
+
+            //close connection
+            closeConnection();
+
+            return dataSet;
+        }
+
+        //create a patient record in the database
+        public void register(string sqlQuery)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = sqlQuery;
+            openConnection();
+            command.Connection = connectionToDB;
+            command.ExecuteNonQuery();
+            closeConnection();
+        }
+
+
+        /*
+        * Insert the name and age into the DB
+        */
+        public void insert(string sqlQuery, String name, int age)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = sqlQuery;
+            command.Parameters.Add(new SqlParameter("Name", name));
+            command.Parameters.Add(new SqlParameter("Age", age));
+
+            openConnection();
+            command.Connection = connectionToDB;
+
+            int noRows = command.ExecuteNonQuery();
+
+            closeConnection();
+
+            Console.WriteLine("n-" + noRows);
+        }
+
+    }
+}
---
+++ Patient_registration.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Patient_registration.xaml
+    /// </summary>
+    public partial class Patient_registration : Window
+    {
+        public Patient_registration()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //code taken from https://stackoverflow.com/questions/4503542/check-for-special-characters-in-a-string
+            if (Name.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)) && Surname.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c))
+                && Date_of_birth.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c) || c == '/') && Address_street.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c))
+                && Address_city.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)) && Address_postcode.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c))
+                && Phone.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)) && Phone_emergency.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)))
+            {
+                string sqlQuery;
+                sqlQuery = @"INSERT INTO 'Patients' ('Patient ID', 'Patient surname', 'Patient date of birth', 'Patient street', 
+                        'Patient city', 'Patient postcode', 'Patient phone number', 'Emergency contact', 'Patient email') VALUES 
+                        (NULL, '" + Name.Text + "', '" + Surname.Text + "', '" + Date_of_birth.Text + "', '" + Address_street.Text + "', '"
+                            + Address_city.Text + "', '" + Address_postcode.Text + "', '" + Phone.Text + "', '" + Phone_emergency.Text + "', '"
+                            + Mail.Text + "')";
+                DBConnection connection = DBConnection.getDBConnectionInstance();
+                connection.register(sqlQuery);
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
