8793bd4b57b48b227d6ddc3b91cf5119a0375797
GiovanniPug <33149451+GiovanniPug@users.noreply.github.com>
2017-11-06 16:22:00 +0000

    Add files via upload

--- WpfApp2/WpfApp2/Patient_registration.xaml.cs
+++ WpfApp2/WpfApp2/Patient_registration.xaml.cs
@@ -26,7 +26,21 @@ namespace WpfApp2
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
+            //code taken from https://stackoverflow.com/questions/4503542/check-for-special-characters-in-a-string
+            if (Name.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)) && Surname.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c))
+                && Date_of_birth.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c) || c == '/') && Address_street.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c))
+                && Address_city.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)) && Address_postcode.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c))
+                && Phone.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)) && Phone_emergency.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)))
+            {
+                string sqlQuery;
+                sqlQuery = @"INSERT INTO 'Patients' ('Patient ID', 'Patient surname', 'Patient date of birth', 'Patient street', 
+                        'Patient city', 'Patient postcode', 'Patient phone number', 'Emergency contact', 'Patient email') VALUES 
+                        (NULL, '" + Name.Text + "', '" + Surname.Text + "', '" + Date_of_birth.Text + "', '" + Address_street.Text + "', '"
+                            + Address_city.Text + "', '" + Address_postcode.Text + "', '" + Phone.Text + "', '" + Phone_emergency.Text + "', '"
+                            + Mail.Text + "')";
+                DBConnection connection = DBConnection.getDBConnectionInstance();
+                connection.register(sqlQuery);
+            }
         }
 
         private void textBox_TextChanged(object sender, TextChangedEventArgs e)
