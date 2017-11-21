33656565febce1da8d437f4b9521d55a38091d69
aleen <aleen@DESKTOP-KBDTU4N>
2017-11-21 10:43:06 +0000

    changed the layout of staff individual, added text boxes

---
+++ WpfApp2/WpfApp2/Staff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Staff
+    {
+        public static void viewStaff()
+        {
+            string query = "SELECT * FROM Staff WHERE Staff_Id = 1";
+            
+            
+            //connect to database
+            DBConnection connection = DBConnection.getDBConnectionInstance();
+            DataSet DataLogin = connection.getDataSet(query);
+
+
+            //tb_staff_id.Text = ["Staff_Id"].ToString();
+        }
+    }
+}
