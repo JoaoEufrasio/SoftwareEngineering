766bbc7ae8c3e92f060501d42448e133e0ed2b7a
GiovanniPug <33149451+GiovanniPug@users.noreply.github.com>
2017-11-21 11:36:59 +0000

    Add files via upload

--- WpfApp2/WpfApp2/main_screen.xaml.cs
+++ WpfApp2/WpfApp2/main_screen.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +23,13 @@ namespace WpfApp2
         public main_screen()
         {
             InitializeComponent();
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            date = date.Split(' ')[0];
+            string sqlQuery = @"SELECT Patients.Patient_ID, Patients.Patient_name, Patients.Patient_surname, Bookings.Booking_ID, Bookings.Time, Bookings.Room FROM Patients INNER JOIN Bookings ON Patients.Patient_ID = Bookings.Patient_ID WHERE Bookings.Booking_date = '" + date + "'";
+            DBConnection connection = DBConnection.getDBConnectionInstance();
+            DataSet todayPatients = connection.getDataSet(sqlQuery);
+            //int count = todayPatients.Tables[0].Rows.Count;
+            
         }
 
         private void bt_bookings_Click(object sender, RoutedEventArgs e)
@@ -46,6 +54,37 @@ namespace WpfApp2
         {
             Patient_registration frm = new Patient_registration();
             frm.Show();
+        }
+
+        private void button_searchPatient(object sender, RoutedEventArgs e)
+        {
+            if (search_patient.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)))
+            {
+                //stores what's in the Searchbox in a variable
+                string id = search_patient.Text;
+                //creates the SQL query
+                string query = @"SELECT * FROM Patients WHERE Patient_ID = '" + id + "';";
+
+                DBConnection connection = DBConnection.getDBConnectionInstance();
+                DataSet patientData = connection.getDataSet(query);
+                //checks that the query returned exactly one result
+                int count = patientData.Tables[0].Rows.Count;
+
+
+                if (count == 1)
+                {
+                    individual_patient patient = new individual_patient(id, patientData);
+                    patient.Show();
+                }
+                else
+                {
+                    MessageBox.Show("No patient found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No patient found.");
+            }
         }
     }
 }
