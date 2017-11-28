892564c9f7e22887acde118d4e3e435b7857cd9f
Aleena Joseph <32543685+aleena-joseph@users.noreply.github.com>
2017-11-28 12:41:53 +0000

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Windows.Controls;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WpfApp2
+{
+    static class Patient
+    {
+        public static void searchPatient(string id)
+        {
+            //creates the SQL query
+            string query = @"SELECT * FROM Patients WHERE Patient_Id = '" + id + "';";
+            DBConnection connection = DBConnection.getDBConnectionInstance();
+            DataSet patientData = connection.getDataSet(query);
+            //checks that the query returned exactly one result
+            int count = patientData.Tables[0].Rows.Count;
+
+            if (count == 1)
+            {
+                individual_patient patient = new individual_patient(id, patientData);
+                patient.Show();
+            }
+            else
+            {
+                MessageBox.Show("No patient found.");
+            }
+        } 
+
+        public static void searchPatientName(string[] name)
+        {
+
+            //create sql statement
+            DBConnection connection = DBConnection.getDBConnectionInstance();
+            if (name[2].Contains('/'))
+            {
+                string sqlQuery = @"SELECT * FROM Patients WHERE Patient_name = '" + name[0] + "' AND Patient_surname = '" + name[1] + "' AND Patient_date_of_birth = '" + name[2] + "';";
+                DataSet patientData = connection.getDataSet(sqlQuery);
+                //checks that the query returned exactly one result
+                int count = patientData.Tables[0].Rows.Count;
+
+                if (count == 1)
+                {
+                    individual_patient patient = new individual_patient(patientData.Tables[0].Rows[0].Field<int>("Patient_Id").ToString(), patientData);
+                    patient.Show();
+                }
+                else
+                {
+                    MessageBox.Show("No patient found.");
+                }
+            }
+            else
+            {
+
+
+                string query = @"SELECT * FROM Patients WHERE Patient_name = '" + name[0] + "' AND Patient_surname = '" + name[1] + "' AND Patient_postcode = '" + name[2] + "';";
+                DataSet patientDataPostcode = connection.getDataSet(query);
+                //checks that the query returned exactly one result
+                int count = patientDataPostcode.Tables[0].Rows.Count;
+
+                if (count == 1)
+                {
+                    individual_patient patient = new individual_patient(patientDataPostcode.Tables[0].Rows[0].Field<int>("Patient_Id").ToString(), patientDataPostcode);
+                    patient.Show();
+                }
+                else
+                {
+                    MessageBox.Show("No patient found.");
+                }
+            }
+            
+            
+            
+        }
+
+        public static DataTable patientsGrid()
+        {
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            date = date.Split(' ')[0];
+            string sqlQuery = @"SELECT Patients.Patient_ID AS ID, Patients.Patient_name AS Name, Patients.Patient_surname AS Surname, Patients.Patient_phone_number AS Number, Staff.Staff_surname AS Doctor FROM Patients INNER JOIN Bookings ON Patients.Patient_ID = Bookings.Patient_ID INNER JOIN Staff ON Bookings.Staff_ID = Staff.Staff_ID WHERE Bookings.Booking_date = '" + date + "'";
+            DBConnection connection = DBConnection.getDBConnectionInstance();
+            DataSet todayPatients = connection.getDataSet(sqlQuery);
+            DataTable dt = todayPatients.Tables[0];
+            return dt;
+        }
+    }
+}
---
+++ main_screen.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for main_screen.xaml
+    /// </summary>
+    public partial class main_screen : Window
+    {
+        public main_screen()
+        {
+            InitializeComponent();
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            date = date.Split(' ')[0];
+            string sqlQuery = @"SELECT Patients.Patient_ID AS ID, Patients.Patient_name AS Name, Patients.Patient_surname AS Surname, Bookings.Booking_ID AS Booking, Bookings.Time, Bookings.Room FROM Patients INNER JOIN Bookings ON Patients.Patient_ID = Bookings.Patient_ID WHERE Bookings.Booking_date = '" + date + "'";
+            DBConnection connection = DBConnection.getDBConnectionInstance();
+            DataSet todayPatients = connection.getDataSet(sqlQuery);
+            DataTable dt = todayPatients.Tables[0];
+            dataGrid.ItemsSource = dt.DefaultView;
+            //int count = todayPatients.Tables[0].Rows.Count;
+            
+        }
+
+        private void bt_bookings_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            booking_screen frm = new booking_screen();
+            frm.Show();
+            this.Close();
+        }
+
+        private void bt_patients_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Patients frm = new Patients();
+            frm.Show();
+            this.Close();
+        }
+
+        private void bt_staff_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            staff_menu frm = new staff_menu();
+            frm.Show();
+            this.Close();
+        }
+
+        private void bt_register_patient_Click(object sender, RoutedEventArgs e)
+        {
+            Patient_registration frm = new Patient_registration();
+            frm.Show();
+        }
+
+        private void button_searchPatient(object sender, RoutedEventArgs e)
+        {
+           
+                if (search_patient.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)))
+                {
+                    //stores what's in the Searchbox in a variable
+                    string id = search_patient.Text;
+                if (search_patient.Text.Contains(' '))
+                {
+                    string[] data = search_patient.Text.Split(' ');
+                    Patient.searchPatientName(data);
+
+                }
+                else
+                {
+                    Patient.searchPatient(id);
+                }
+
+
+
+                }
+                else
+                {
+                    MessageBox.Show("No patient found.");
+                }
+            }
+
+        private void button_searchBooking(object sender, RoutedEventArgs e)
+        {
+            if (search_booking.Text.Any(c => Char.IsLetterOrDigit(c) || Char.IsWhiteSpace(c)))
+            {
+                //stores what's in the Searchbox in a variable
+                string id = search_booking.Text;
+                //creates the SQL query
+                string query = @"SELECT * FROM Bookings WHERE Booking_Id = '" + id + "';";
+
+                DBConnection connection = DBConnection.getDBConnectionInstance();
+                DataSet bookingData = connection.getDataSet(query);
+                //checks that the query returned exactly one result
+                int count = bookingData.Tables[0].Rows.Count;
+
+                if (count == 1)
+                {
+                    booking_screen booking = new booking_screen(id, bookingData);
+                    booking.Show();
+                }
+                else
+                {
+                    MessageBox.Show("No booking found.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No booking found.");
+            }
+        }
+
+        private void search_booking_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void search_booking_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
